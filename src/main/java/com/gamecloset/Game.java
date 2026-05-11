8708e8a0dd744e1995890d1478fcc6f72c4b0a43
diogompintoo <diogomartinsp1992@gmail.com>
2026-05-11 19:26:15 +0100

    fix naming (genre not gender)

--- src/main/java/com/gamecloset/Game.java
+++ src/main/java/com/gamecloset/Game.java
@@ -2,7 +2,7 @@ package com.gamecloset;
 
 public class Game {
     private String title;
-    private String gameGender;
+    private String gameGenrer;
     private String platform;
     private int year;
     private String condition;
@@ -10,20 +10,20 @@ public class Game {
 
     public Game (String title, String gameGender, String platform, int year, String condition, double price) {
         this.title = title;
-        this.gameGender = gameGender;
+        this.gameGenrer = gameGender;
         this.platform = platform;
         this.year = year;
         this.condition = condition;
         this.price = price;
     }
     public String getTitle () { return title; }
-    public String getGenre () { return gameGender; }
+    public String getGenre () { return gameGenrer; }
     public String getPlatform() { return platform; }
     public int getYear() { return year; }
     public String getCondition() { return condition; }
     public double getPrice() { return price; }
 
     public String toString() {
-        return String.format(" %s (%s - %s) | %d | %s | €%.2f", title, gameGender, platform, year, condition, price);
+        return String.format(" %s (%s - %s) | %d | %s | €%.2f", title, gameGenrer, platform, year, condition, price);
     }
 }
